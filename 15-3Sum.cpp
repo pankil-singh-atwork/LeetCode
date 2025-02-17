a272558753cf4810e7d29ecfaadfc1907647d734
Pankil Singh <86354500+pankil-singh-atwork@users.noreply.github.com>
2025-02-17 20:17:46 +0530

    [15] [Time Beats: 61.16%] [Memory Beats: 30.18%] - LeetPush

--- 15-3Sum.cpp
+++ 15-3Sum.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        // using two pointer -> optimal approach
-        set<vector<int>> res;
+        vector<vector<int>> ans;
+        // using two pointer(without set data structure) -> optimal approach
         int n = nums.size();
+        sort(nums.begin(), nums.end());
         // two-pointer approach
-        sort(nums.begin() , nums.end());
         for (int i = 0; i < n - 2; i++) {
+            // condition to skip the duplicate ith element -> triplets
+            if (i != 0 && nums[i] == nums[i - 1])
+                continue;
             int j = i + 1;
             int k = n - 1;
             while (j < k) {
@@ -17,15 +20,19 @@ public:
                 } else if (target > 0) {
                     k--;
                 } else {
-                    // generate the triplet
+                    // generate the unique triplet
                     vector<int> temp = {nums[i], nums[j], nums[k]};
-                    res.insert(temp);
+                    ans.push_back(temp);
                     j++;
                     k--;
+                    // condition to skip the duplicates
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++;
+                    while (j < k && nums[k] == nums[k + 1])
+                        k--;
                 }
             }
         }
-        vector<vector<int>> ans(res.begin(), res.end());
         return ans;
     }
 };
