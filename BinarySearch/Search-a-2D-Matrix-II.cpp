7e0bdd394f80b8f9d0238f139ff0d3cb5c2d095f
Pankil Singh <86354500+pankil-singh-atwork@users.noreply.github.com>
2025-03-14 16:49:33 +0530

    [240] [Time Beats: 96.23%] [Memory Beats: 67.59%] - LeetPush

--- BinarySearch/Search-a-2D-Matrix-II.cpp
+++ BinarySearch/Search-a-2D-Matrix-II.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    bool binarySearch(vector<int>& arr, int target) {
-        int low = 0;
-        int high = arr.size() - 1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (arr[mid] == target)
-                return true;
-            else if (arr[mid] < target)
-                low = mid + 1;
-            else
-                high = mid - 1;
-        }
-        return false;
-    }
+    // optimal approach -> fiing the top right corner & moving 
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        bool isFound = false;
-        for (int i = 0; i < matrix.size(); ++i) {
-            if (binarySearch(matrix[i], target)) {
-                // if element found no need to search in more rows;
-                isFound = true;
-                break;
+        int top = 0;
+        int right = matrix[top].size() - 1; // m - 1
+        int n = matrix.size(); 
+        // traverse the matrix 
+        while (top <= n-1 && right >= 0) {
+            // keep finding the element
+            if (target < matrix[top][right]) {
+                // move left
+                --right;
+            } else if (target > matrix[top][right]) {
+                // move down
+                ++top;
+            } else {
+                return true;
             }
         }
-        return isFound;
+        return false;
     }
 };
